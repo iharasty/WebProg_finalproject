bac72cc14f2848c51a44548aa4d6eb59bdf359f2
Isaac Harasty <harastyj6@students.rowan.edu>
2021-04-16 20:50:16 -0400

    made db_dumps/, as well as login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+
+>
+<html>
+	<head>
+
+    </head>
+
+    <body>
+
+        <form action='./login.php' action='GET'>
+        
+            <label for='email'>Email</label>
+            <input type='text' id='email' name = 'email' value=''
+            />
+
+            <label for='password'>Password</label>
+            <input type='text' id='password' name = 'password' value=''
+            />
+        
+            
+
+        </form>
+
+
+    </body>
+</html>
